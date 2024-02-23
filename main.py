96974f24b0722c7799accea26f61645b87c09b41
superuoa <superuoa@gmail.com>
2024-02-23 12:02:35 +0700

    ีupdate show os

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+# Print Hello from the current OS and architecture
+#
+# python hello_world.py
+
+
+import platform
+
+
 if __name__ == '__main__':
-    print('Hello Docker world! ทดสอบภาษาไทย')
+    # Get the operating system name
+    os_name = platform.system()
+
+    # Get the system's architecture
+    architecture = platform.architecture()
+
+    print(f"Hello Wordl! from {os_name} ทดสอบภาษาไทย")
+else:
+    pass
