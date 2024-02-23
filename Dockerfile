bc6009b1c61707a9ca6239cc8ef663ad5d2bc2c5
superuoa <superuoa@gmail.com>
2024-02-23 12:17:11 +0700

    update phy ocp

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 # Use an official Python runtime as a parent image
 FROM registry.redhat.io/rhel9/python-39@sha256:4da8ddb12096a31d8d50e58ea479ba2fe2f252f215fbaf5bf90923a1827463ba
 
-# Set the working directory in the container
-WORKDIR /app
+RUN apk add --no-cache py3-pip \
+    && pip3 install --upgrade pip
 
-# Copy the local directory contents into the container at /app
+WORKDIR /app
 COPY . /app
 
-# Run Python script when the container launches
-CMD ["python", "main.py"]
+RUN pip3 --no-cache-dir install -r requirements.txt
+
+EXPOSE 5000
+
+ENTRYPOINT ["python3"]
+CMD ["main.py"]
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# Print Hello from the current OS and architecture
-#
-# python hello_world.py
+'''
+Created on 04-Sep-2019
 
+@author: bkadambi
+'''
 
-import platform
+# -*- coding: UTF-8 -*-
+"""
+hello_flask: First Python-Flask webapp
+"""
+from flask import Flask  # From module flask import class Flask
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    """Say hello"""
+    return 'Hello, world! ทดสอบภาษาไทย'
 
-if __name__ == '__main__':
-    # Get the operating system name
-    os_name = platform.system()
-
-    # Get the system's architecture
-    architecture = platform.architecture()
-
-    print(f"Hello Wordl! from {os_name} ทดสอบภาษาไทย")
-else:
-    pass
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello World! Built with a Docker file. ทดสอบภาษาไทย")
+    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
